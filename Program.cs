60f096f934ff4f26c63a5fff14dee6edf129dca7
Jens Palmqvist <jpalmqvi@gmail.com>
2024-02-09 12:04:30 +0100

    Added components and stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BlazorCodeAlong;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PokeApiNet;
@@ -8,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new PokeApiClient());
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
